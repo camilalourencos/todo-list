e89fbcb2d981a1c9c73d6332d21d8e65c7ac0f01
Camila Lourenco de Souza <Camila.Lourenco@ibm.com>
2022-01-19 23:14:24 -0300

    styling

--- src/components/NoteCard/NoteCard.jsx
+++ src/components/NoteCard/NoteCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaRegTrashAlt, AiOutlineCheckCircle } from 'react-icons/fa';
+import { FaRegTrashAlt } from 'react-icons/fa';
 import "./style.css";
 
 export function NoteCard({ notes, onRemove }) {
@@ -10,22 +10,23 @@ export function NoteCard({ notes, onRemove }) {
             <header className='note-card_header'>
                 <h3 className='note-card_description'>{notes.description}</h3>
             </header>
-            <div className='completed-checkbox'> 
-                <input
-                    type='checkbox'
-                    defaultChecked={notes.completed}
-                    className='form-check-input'
-                    onChange={() => setStatus(true)}
-                />
-                <label class='checked'>Completed</label>
+            <div className='actions-buttons'>
+                <label className='container'> 
+                    <input
+                        type='checkbox'
+                        defaultChecked={notes.completed}
+                        onChange={() => {setStatus(true)}}
+                    />
+                    <span className='checkmark'></span>
+                </label>
+                <button 
+                    className='form-delete-input'
+                    onClick={() => onRemove(notes.id)}
+                >
+                    <FaRegTrashAlt/>
+                </button>
             </div>
-            <button 
-                className='form-delete-input'
-                onClick={() => onRemove(notes.id)}
-            >
-                <FaRegTrashAlt/>
-            </button>
-            </section>
-    );
 
+        </section>
+    );
 }
